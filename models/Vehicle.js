216892092a66395daa9003c9ef392e0a3dcb9dcc
anirudhkaluri <anirudhkaluri@gmail.com>
2023-03-05 20:10:42 +0530

    Vehicle model created

---
+++ models/Vehicle.js
@@ -0,0 +1,18 @@
+const User=require("User");
+module.exports=(sequelize,DataTypes)=>{
+    const Vehicle=sequelize.define("Vehicle",{
+        user_id:{
+            type:DataTypes.INTEGER,
+            allowNull:false,
+            references:{
+                model:User,
+                key:user_id
+            }
+        },
+        vehicle_production_date:{
+            type:DataTypes.INTEGER,
+            allowNull:false
+        }
+    });
+    return Vehicle;
+}
