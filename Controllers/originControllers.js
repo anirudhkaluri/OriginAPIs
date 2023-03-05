1c8c37fbb4aa249f9c5d20817e78875db52d29b9
anirudhkaluri <anirudhkaluri@gmail.com>
2023-03-05 20:24:18 +0530

    defined controllers, not implemented yet, exported the functions from originControllers.js

---
+++ Controllers/originControllers.js
@@ -0,0 +1,15 @@
+const {User,Vehicle,Housing}=require('../models');
+
+
+const create_user=(req,res)=>{
+
+}
+
+const calculate_risk=(req,res)=>{
+
+}
+
+module.exports={
+    create_user,
+    calculate_risk
+}
