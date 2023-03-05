849d0d09eb191940478f11a8f3837d99755f3096
anirudhkaluri <anirudhkaluri@gmail.com>
2023-03-05 20:15:21 +0530

    apiRoutes imported by app.js

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express=require("express");
 const app=express();
 
+const apiRoutes=require("./Routes/apiRoutes");
+
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(apiRoutes);
+
 
 
 
