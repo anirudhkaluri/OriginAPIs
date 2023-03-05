b67027f767fc9a1d1cc35c36a36775b2cf787774
anirudhkaluri <anirudhkaluri@gmail.com>
2023-03-05 20:06:32 +0530

    created the Housing model

---
+++ models/Housing.js
@@ -0,0 +1,21 @@
+const User=require("User");
+module.exports=(sequelize,DataTypes)=>{
+    const Housing=sequelize.define("Housing",{
+        user_id:{
+            type:DataTypes.INTEGER,
+            allowNull:false,
+            references:{
+                model:User,
+                key: user_id,
+            }
+        },
+        ownership_status:{
+            type:DataTypes.STRING,
+            validate:{
+                isIn:[['owned','mortgaged']]
+            }
+
+        }
+    });
+    return Housing;
+}
