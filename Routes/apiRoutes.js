e656c133e83cdf1a57ef5aafb93e40471fd11113
anirudhkaluri <anirudhkaluri@gmail.com>
2023-03-05 20:29:59 +0530

    routes configured in apiRoutes.js. all routes implemented in apiRoutes.js

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -2,6 +2,8 @@ const express=require('express');
 const router=express.Router();
 const originController=require('../Controllers/originControllers');
 
+router.post('/createUser',originController.create_user);
 
+router.get('/calculateRisk/:user_id',originController.calculate_risk);
 
 module.exports=router;
