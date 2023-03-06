d14df76ebbdc95a40167200d89ed7f31ef42d583
anirudhkaluri <anirudhkaluri@gmail.com>
2023-03-06 09:48:07 +0530

    implemented the calculate_disability_risk logic

--- Controllers/calculateRisk.js
+++ Controllers/calculateRisk.js
@@ -16,6 +16,23 @@ const calculate_auto_risk=(user)=>{
 
 
 const calculate_disability_risk=(user)=>{
+    if(user.income===0 || user.possess_vehicle===false || user.possess_house===false ||user.Age>60)
+        return "ineligible";
+    var base_risk=parseInt(user.risk_answers[0])+parseInt(user.risk_answers[1])+parseInt(user.risk_answers[2]);
+    if(user.Age<30) base_risk=base_risk-2;
+    if(user.Age>30 && user.Age<40) base_risk=base_risk-1;
+    if(user.income>200000) base_risk=base_risk-1;
+    const ownership_status=get_house_ownership(user); //TO-DO implement getOwnership, export, and import it here
+    if(owernship_status==='mortgaged')
+        base_risk=base_risk+1;
+    if(user.dependents_count>0)
+        base_risk=base_risk+1;
+    if(user.marital_status==='married')
+        base_risk=base_risk-1;
+    if(base_risk<=0)    return "economic";
+    if(base_risk===1 || base_risk===2) return "regular";
+    if(base_risk===3) return "responsible";
+    return "ineligible";
     
 
 }
