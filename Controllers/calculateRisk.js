c7293cffe04ace54965fc4a33fe56a56072dab1d
anirudhkaluri <anirudhkaluri@gmail.com>
2023-03-06 09:40:52 +0530

    implemented calculate_auto_risk feature in calculateRisk.js

--- Controllers/calculateRisk.js
+++ Controllers/calculateRisk.js
@@ -1,11 +1,23 @@
+const {Vehicle,Housing}= require('../models');
+
 
 const calculate_auto_risk=(user)=>{
-
+    if(user.income===0 || user.possess_vehicle===false || user.possess_house===false)
+        return "ineligible";
+    var base_risk=parseInt(user.risk_answers[0])+parseInt(user.risk_answers[1])+parseInt(user.risk_answers[2]);
+    if(user.Age<30) base_risk=base_risk-2;
+    if(user.Age>30 && user.Age<40) base_risk=base_risk-1;
+    if(user.income>200000) base_risk=base_risk-1;
+    if(base_risk<=0)    return "economic";
+    if(base_risk===1 || base_risk===2) return "regular";
+    if(base_risk===3) return "responsible";
+    return "ineligible";
 }
 
 
 const calculate_disability_risk=(user)=>{
     
+
 }
 
 
